e8afc249a0165e07a083dd424791d67c6f0bf548
JSWhiteOG <jswhite@gmail.com>
2023-10-01 23:20:26 -0700

    jdas

--- src/App.js
+++ src/App.js
@@ -18,22 +18,22 @@ const App = () => {
     }
 }, [searchTerm])
 
-const renderGallery = () => {
-    if(data) {
-        return (
-            <Suspense fallback={<Spinner />}>
-                <Gallery data={data} />
-            </Suspense>
-        )
-    }
-}
+
 
 
   const handleSearch = (e, term) => {
       e.preventDefault()
       setSearch(term)
   }
-
+const renderGallery = () => {
+    if(data) {
+        return (
+            <Suspense fallback={<Spinner/>}>
+                <Gallery data={data} />
+            </Suspense>
+        )
+    }
+}
   return (
     <div style={{background:'black', color:'white', fontSize:'25px', flex:1, textAlign:'center'}}>
         <SearchBar handleSearch={handleSearch} />
